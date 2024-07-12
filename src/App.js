36dfbf3cb5f0aefddf2beaf0cbbc2c60c68e6497
Rakxit121 <rakxit121@outlook.com>
2024-07-12 12:21:57 +0545

    gym_change

--- src/App.js
+++ src/App.js
@@ -1,106 +1,70 @@
-import { useEffect } from "react";
-import {
-  Routes,
-  Route,
-  useNavigationType,
-  useLocation,
-} from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import Payment from "./pages/Payment";
-import AdminMembers from "./pages/AdminMembers";
-import Membership from "./pages/Membership";
-import Schedule from "./pages/Schedule";
-import Trainers from "./pages/Trainers";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Trainers1 from "./pages/Trainers1";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import Navbar from "./components/Nav/Navbar";
+
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import Classes from "./Pages/Classes";
+import Price from "./Pages/Price";
+import Blog from "./Pages/Blog";
+import Schedule from "./Pages/Schedule/Schedule";
+import Monday from "./Pages/Schedule/Monday";
+import Tuesday from "./Pages/Schedule/Tuesday";
+import Wednesday from "./Pages/Schedule/Wednesday";
+import Thursday from "./Pages/Schedule/Thursday";
+import Friday from "./Pages/Schedule/Friday";
+import Saturday from "./Pages/Schedule/Saturday";
+import Sunday from "./Pages/Schedule/Sunday";
+import Gallery from "./Pages/Gallery/Gallery";
+import GalleryPage1 from "./Pages/Gallery/GalleryPage1";
+import GalleryPage2 from "./Pages/Gallery/GalleryPage2";
+import About from "./Pages/About";
 
 function App() {
-  const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
-
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
-
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
-
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/payment":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/adminmembers":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/membership":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/schedule":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/trainers":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/contact":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/login-1":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/signup-1":
-        title = "";
-        metaDescription = "";
-        break;
-      case "/trainers1":
-        title = "";
-        metaDescription = "";
-        break;
-    }
-
-    if (title) {
-      document.title = title;
-    }
-
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
-
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/payment" element={<Payment />} />
-      <Route path="/adminmembers" element={<AdminMembers />} />
-      <Route path="/membership" element={<Membership />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/trainers" element={<Trainers />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/login-1" element={<Login />} />
-      <Route path="/signup-1" element={<Signup />} />
-      <Route path="/trainers1" element={<Trainers1 />} />
-    </Routes>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="classes" element={<Classes />} />
+          <Route path="pricing" element={<Price />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="about" element={<About />} />
+
+          <Route path="schedule" element={<Schedule />}>
+            <Route path="monday" element={<Monday />} />
+            <Route path="tuesday" element={<Tuesday />} />
+            <Route path="wednesday" element={<Wednesday />} />
+            <Route path="thursday" element={<Thursday />} />
+            <Route path="friday" element={<Friday />} />
+            <Route path="saturday" element={<Saturday />} />
+            <Route path="sunday" element={<Sunday />} />
+          </Route>
+
+          <Route path="gallery" element={<Gallery />}>
+            <Route path="page-1" element={<GalleryPage1 />} />
+            <Route path="page-2" element={<GalleryPage2 />} />
+          </Route>
+
+          <Route path="admin" element={<Admin/>}>
+            {/* Admin routes */}
+            <Route path="dashboard" element={<h1>Admin Dashboard</h1>} />
+            <Route path="classes" element={<h1>Admin Classes</h1>} />
+            <Route path="blog" element={<h1>Admin Blog</h1>} />
+            <Route path="schedule" element={<h1>Admin Schedule</h1>} />
+            <Route path="gallery" element={<h1>Admin Gallery</h1>} />
+            <Route path="settings" element={<h1>Admin Settings</h1>} />
+          </Route>
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
+
 export default App;
