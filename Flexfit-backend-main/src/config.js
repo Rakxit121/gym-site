931f845f0ade971b925219a41d6f6f06ef612b22
Rakxit121 <rakxit121@outlook.com>
2024-07-14 13:05:50 +0545

    backend js config

---
+++ Flexfit-backend-main/src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: process.env.PORT || 3000,
+    dbUri: process.env.DB_URI || 'mongodb+srv://test:test@test0.0nae7tz.mongodb.net/',
+    jwtSecret: process.env.JWT_SECRET || 'randjwtcode1238yfudfoldflknfdv',
+};
