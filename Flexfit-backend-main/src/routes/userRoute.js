4ed10f14a66706315bbb63352b6bf464a6481e30
Rakxit121 <rakxit121@outlook.com>
2024-07-14 13:21:11 +0545

    routing users

--- Flexfit-backend-main/src/routes/userRoute.js
+++ Flexfit-backend-main/src/routes/userRoute.js
@@ -1,10 +1,13 @@
-import express from "express";
-import { register, login, getCurrentUser, registerAClass } from "../controllers";
-import { validateToken } from "../middlewares/validateToken";
-export const userRoute = express.Router();
+const express = require("express");
+const { register, login, getCurrentUser, registerAClass } = require("../controllers");
+const { validateToken } = require("../middlewares/validateToken");
+
+const userRoute = express.Router();
 
 userRoute.route("/signup").post(register);
 userRoute.route("/login").post(login);
 userRoute.route("/current").get(validateToken, getCurrentUser);
 
 userRoute.route("/registerAClass").post(registerAClass);
+
+module.exports = { userRoute };
